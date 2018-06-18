e1fd6142c966d0053ec2e0cc5b686ab0744e6f37
stalary <452024236@qq.com>
2018-06-18 17:07:59 +0800

    init

---
+++ src/test/java/com/stalary/lightmq/LightmqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stalary.lightmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LightmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
